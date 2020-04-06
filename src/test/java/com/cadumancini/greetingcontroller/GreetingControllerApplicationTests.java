a02c21db450011defd70e75bd0ac31642616f5a0
cadumancini <cadumancini@gmail.com>
2020-04-06 15:19:49 -0300

    Adding Tests

--- src/test/java/com/cadumancini/greetingcontroller/GreetingControllerApplicationTests.java
+++ src/test/java/com/cadumancini/greetingcontroller/GreetingControllerApplicationTests.java
@@ -1,13 +1,35 @@
 package com.cadumancini.greetingcontroller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GreetingControllerApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").value("Hello, World!"));
+	}
+
+	@Test
+	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+		this.mockMvc.perform(get("/greeting").param("name","Cadu"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.content").value("Hello, Cadu!"));
 	}
 
 }
